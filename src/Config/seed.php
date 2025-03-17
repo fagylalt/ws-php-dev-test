78932200f0e7d3afb8f903a0d9e11009cfbf0823
Kálvin Ferenc <kalvin.ferenc@zenit.hu>
2025-03-17 22:07:25 +0100

    feat: now we can seed data into the created database directly

---
+++ src/Config/seed.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'bootstrap.php';
+use Seeders\ClientSeeder;
+use Seeders\UserClientSeeder;
+use Seeders\UserSeeder;
+
+ClientSeeder::seed(10);
+echo "Seeded 10 clients\n";
+
+UserSeeder::seed(10);
+echo "Seeded 10 users\n";
+
+UserClientSeeder::seed();
+echo "Seeded 10 user-client relationships\n";
